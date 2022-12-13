02ff4e9b617e4af25633775767dd14fa1f2ab073
Marcos Silva de Souza <117598188+msouza4@users.noreply.github.com>
2022-12-12 21:04:09 -0300

    CSS

---
+++ css/main.js
@@ -0,0 +1,24 @@
+function tocaSom (idElementoAudio){
+	document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+
+while(contador < listaDeTeclas.length){
+
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    console.log(instrumento);
+
+	listaDeTeclas[contador].onclick = function (){
+        tocaSom('#som_tecla_pom');
+	}
+
+	contador = contador + 1;
+
+	console.log(contador);
+
+}
